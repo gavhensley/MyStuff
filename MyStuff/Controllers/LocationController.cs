8fe9fbbfb0ee7dfa95dcc7c07f8debedfeacacd4
gavhensley <gavin@wecancodeit.org>
2022-09-02 18:02:44 -0400

    take 2

--- MyStuff/Controllers/LocationController.cs
+++ MyStuff/Controllers/LocationController.cs
@@ -27,7 +27,7 @@ namespace MyStuff.Controllers
 
         // POST: LocationController/Create
         [HttpPost]
-        //[ValidateAntiForgeryToken]
+        //[ValidateAntiForgeryToken] I will change this back in a second
         public IActionResult Create(Location location)
         {
             //keep and eye on this try catch. 
